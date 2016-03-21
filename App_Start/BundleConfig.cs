924b0e6987cbc86ced0d18960e25876b2fd08d97
andreiamtf <andreiamtf@gmail.com>
2016-03-21 12:21:45 +0000

    App_Start

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace TaskMA
+{
+    public class BundleConfig
+    {
+        
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap-formhelpers-phone.js",
+                      "~/Scripts/bootstrap-datepicker.js"
+                      ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                       "~/Content/Site.css",
+                      "~/Content/bootstrap-datepicker.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/bundles/datetime").Include(
+                      "~/Scripts/moment*",
+                      "~/Scripts/bootstrap-datetimepicker*"));
+
+        }
+    }
+}
